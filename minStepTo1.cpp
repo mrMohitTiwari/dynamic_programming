bf2de0a9d71e8135db1d838434080132601b9ab0
Mohit Tiwari <mt0988145@gmail.com>
2026-07-09 12:11:48 +0530

    adding files:

--- minStepTo1.cpp
+++ minStepTo1.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 using namespace std;
+int helper(int n , int *a)
+{
+    if(n<=1) return 0;
+    if(a[n]!=-1) return a[n];
+    int x = helper(n-1,a);
+    int y,z;
+    z=y = INT_MAX;
+if(n%2==0) helper(n/2,a);
+if(n%3==0) helper(n/3,a);
+int ans  = min(x,min(y,z)) +1;
+a[n] = ans;
+return ans;
+}
+int min_steps2(int n)
+{
+    int *a = new int[n+1];
+    for(int i =0;i<n;i++) a[i]=-1;
 
+    int ans = helper(n ,a);
+}
+int min_steps(int n){
+    if(n<=1) return 0;
+    int x = min_steps(n-1);
+    int y , z;
+    y = z =INT_MAX;
+    if(n%2==0) y = min_steps(n/2);
+    if(n%3==0) z = min_steps(n/3);
+    int ans = min(x,min(y,z))+1;
+    return ans;
+}
 int main() {
-    
+    int n ;
+    cin>>n;
+    cout<<min_steps(n)<<"\n";
     return 0;
 }
